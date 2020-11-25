ff33c9890d0a0e4367800f69428320c3c612fabf
Matheus Rosa <matheusrosa.la@gmail.com>
2020-11-24 23:36:34 -0300

    ajustando models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -10,10 +10,13 @@ import {
 class Category {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   title: string;
+
   @CreateDateColumn()
   created_at: Date;
+
   @UpdateDateColumn()
   updated_at: Date;
 }
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,29 +1,39 @@
 import {
   Entity,
-  Column,
   PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
+  Column,
   ManyToOne,
   JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
+
 import Category from './Category';
 
 @Entity('transactions')
 class Transaction {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   title: string;
+
   @Column()
   type: 'income' | 'outcome';
+
   @Column('decimal')
   value: number;
+
   @ManyToOne(() => Category)
-  @JoinColumn({name: 'category_id'})
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
+
   @CreateDateColumn()
   created_at: Date;
+
   @UpdateDateColumn()
   updated_at: Date;
 }
